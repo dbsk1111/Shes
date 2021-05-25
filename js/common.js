389ea2d56b4137afcce8f43daaccba1e89f8d966
dbsk1111 <dbsk1111@gmail.com>
2021-05-25 14:48:01 +0900

    210525
    데코레이션 움직임 완성

--- js/common.js
+++ js/common.js
@@ -1,26 +1,26 @@
 $(function(){
   let dHeight = $(document).height();
   // let wHeight = $(window).innerHeight();
-  // let winScrollTop = $(window).scrollTop();
+  let winScrollTop = $(window).scrollTop();
   // let footerHeight = $('footer').outerHeight();
   // let footerHeight = $('footer').outerHeight();
 
   // 헤더 마우스 호버 시 노출
-  $('header').mouseenter(()=>{
+  $('header').mouseenter(function(){
     $(this).addClass('on')
-  }).mouseleave(()=>{
+  }).mouseleave(function(){
     $(this).removeClass('on')
   })
 
   // 버튼 눌러서 섹션 이동
-  $('header a, .top_button a').click(()=>{
+  $('header a, .top_button a').click(function(){
     let thisElem = $(this.hash);
     let offsetElem = thisElem.offset();
     $('html,body').stop();
     $('html,body').animate({scrollTop : offsetElem.top},400);
   })
 
-  $('#gnb_btn').click(()=>{
+  $('#gnb_btn').click(function(){
     // $('#gnb').slideup();
     if( $('#gnb').css('display') == 'none' ){
       $('header, #gnb_btn').addClass('on')
@@ -37,7 +37,7 @@ $(function(){
     let wHeight = $(window).innerHeight();
     let addHeight = wHeight*1;
     let thisScrollTop = $(this).scrollTop();
-    $(".ts").each(()=>{
+    $(".ts").each(function(){
       let thisOffset = $(this).offset();
       let thisHeight = $(this).outerHeight();
       if( (thisScrollTop + (wHeight * 0.75) ) >= thisOffset.top && (thisOffset.top + thisHeight - (wHeight * 0.45) ) > thisScrollTop ){
@@ -47,14 +47,24 @@ $(function(){
   }
 
   // 화면 커졌을 때 gnb 노출
-  $(window).resize(()=>{
+  $(window).resize(function(){
     if( $(this).innerWidth >= 768 ){
       $('#gnb').css('display','block')
     }
   })
 
+
   // 헤더 스크롤 위치에 따라 노출
-  $(window).scroll(()=>{
+  $(window).scroll(function(){
+    winScrollTop = $(window).scrollTop();
+
+    // 데코레이션 움직임
+    let deco1Height = winScrollTop*0.5;
+    let deco2Height = winScrollTop*0.3;
+    let deco3Height = winScrollTop*0.7;
+    $('.deco1').css('transform', 'translateY(-'+deco1Height+'px)');
+    $('.deco2').css('transform', 'translateY(-'+deco2Height+'px)');
+    $('.deco3').css('transform', 'translateY(-'+deco3Height+'px)');
 
     // ts 클래스 반응
     activeFunc();
@@ -69,7 +79,6 @@ $(function(){
     // 탑 버튼
     // dHeight = $(document).height();
     let wHeight = $(window).innerHeight();
-    winScrollTop = $(window).scrollTop();
     // let footerHeight = $('footer').outerHeight();
     let footerHeight = $('footer').offset().top;
 
@@ -83,11 +92,12 @@ $(function(){
 
   // 말줄임표 처리
   const length = 170;
-  $('.essey_title + p').each(()=>{
+  $('.essey_title + p').each(function(){
     if( $(this).text().length >= length ){
       $(this).text($(this).text().substr(0,length)+'..');
     }
   })
 })
+
 
 // $('').fadeIn();
